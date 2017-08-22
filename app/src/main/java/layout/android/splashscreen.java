53f5ba58b16c4dd5624d3c51b7681b11d0ad4bdc
gaganklair <klairgagandeep@gmail.com>
2017-08-22 19:40:36 +0530

    hello world

---
+++ app/src/main/java/layout/android/splashscreen.java
@@ -0,0 +1,32 @@
+package layout.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.toshiba.jaguar.MainActivity;
+import com.example.toshiba.jaguar.R;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+
+}
